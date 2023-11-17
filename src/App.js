0504de81b26bafe728f115779a9cdb26d589559f
Thippaluri Yaseen Basha <yasenroycey6@gmail.com>
2023-11-17 21:44:53 +0530

    cursor

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.scss';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './components/Home';
+import Cursor from './components/Cursor';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route index element={<Home />} />
       </Route>
     </Routes>
+    <Cursor/>
     </>
   );
 }
---
+++ src/components/Cursor/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import './index.scss'
+import './cursor.js'
+
+export default function Cursor(){
+
+    return (
+        <>
+            <div class="cursor-dot" data-cursor-dot></div>
+            <div class="cursor-outline" data-cursor-outline></div>
+        </>
+    )
+
+}
+
+
